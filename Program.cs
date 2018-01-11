79717e274d06821cea71ba69a906c552c2b3cad0
FieldsOfGreen <jrosales0001@gmail.com>
2018-01-11 14:42:44 -0500

    finished program w/ one error in timespan output

--- Program.cs
+++ Program.cs
@@ -11,37 +11,35 @@ namespace DateCalc
         //***Variable Declarations
         static bool runProgram = true, getFirstDate = true, getSecondDate = true;
         static DateTime firstDate, secondDate;
-        static string firstDateString, secondDateString;
-        
 
         static void Main(string[] args)
         {
             //***User input 2 dates of month/day/year
-            Console.WriteLine("Please enter your first date in the format of MM/DD/YYY");
-            Console.WriteLine("For example: January 1, 2010, should be entered as 01/01/2010.");
+           // Console.WriteLine("Please enter your first date in the format of MM/DD/YYY");
+           // Console.WriteLine("For example: January 1, 2010, should be entered as 01/01/2010.");
             while (runProgram)
             {
                 while (getFirstDate)
-                {                    
+                {
+                    Console.WriteLine("Please enter your first date in the format of MM/DD/YYY");
+                    Console.WriteLine("For example: January 1, 2010, should be entered as 01/01/2010.");
                     if (DateTime.TryParse(Console.ReadLine(), out firstDate))
                     {
                         Console.Clear();
-                        Console.WriteLine("Your first date is: " + firstDate.ToShortDateString());         
+                        Console.WriteLine("Your first date is: " + firstDate.ToShortDateString());
                         getFirstDate = false;
                     }
                     else
                     {
                         Console.Clear();
                         Console.WriteLine("Oops! You need to enter the date in the right format: MM/DD/YYYY");
-                        
                     }
                 }
                 while (getSecondDate)
-                {                   
+                {
                     Console.WriteLine("Please enter another date.");
                     if (DateTime.TryParse(Console.ReadLine(), out secondDate))
-                    {
-                        Console.WriteLine("Your second date is: " + secondDate.ToShortDateString());
+                    {                        
                         getSecondDate = false;
                     }
                     else
@@ -54,53 +52,25 @@ namespace DateCalc
                 Console.Clear();
                 Console.WriteLine("Your first date is: {0}", firstDate.ToShortDateString());
                 Console.WriteLine("Your second date is: {0}", secondDate.ToShortDateString());
-                Console.ReadKey();
-            }
-            
-
-
-            //***Output difference between dates in days/hours/minutes
-            
-            //this creates a new instance of the datetime class AND initialized it to right now.
-            /*
-            DateTime myValue = DateTime.Now;
-            */
-
-            /*
-            Console.WriteLine("month/day/year AND hours/minutes/seconds: " + myValue.ToString());
-            Console.WriteLine("month/day/year: " + myValue.ToShortDateString());
-            Console.WriteLine("long date string" + myValue.ToLongDateString());
-            Console.WriteLine("long time: " + myValue.ToLongTimeString());
-
-            Console.WriteLine("adding days: " + myValue.AddDays(3).ToLongDateString());
-            Console.WriteLine("adding hours: " + myValue.AddHours(3).ToShortTimeString());
-            */
-
-            //***if you want to subtract time, days, etc. 
-            //Console.WriteLine(myValue.AddDays(-3).ToShortDateString());
-
-            //***retrieving certain parts of the date or time...
-            //Console.WriteLine(myValue.Month.ToString());
-            //Console.WriteLine(myValue.);
-
-            //***Creating and initializing date/time objects..
-
-
-            //***ONE way to create a new date is to pass it a series of integers...
-            /*
-            DateTime myBirthday = new DateTime(2005, 5, 25);
-            Console.WriteLine(myBirthday.ToShortDateString());
-            */
-
-            //comparing the difference between two datetime objects using the timespan object
-            /*
-            DateTime myBirthday = DateTime.Parse("12/7/1969");
-            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
-            Console.WriteLine(myAge.TotalDays);
-            */
-
-
-            Console.ReadLine();
+                TimeSpan timeBetween = firstDate - secondDate;
+                
+                //***Output
+                Console.WriteLine("The time between your two dates is {0} days, {1} hours, and {2} minutes.", timeBetween.Duration().Days, timeBetween.Duration().Hours, timeBetween.Duration().Minutes);               
+                
+                Console.WriteLine("\nIf you want to run this program again press the 'y' key.");
+                bool continueProgram;
+                continueProgram = Console.ReadLine().ToLower()[0].Equals('y');
+                if (continueProgram)
+                {
+                    getFirstDate = true;
+                    getSecondDate = true;
+                    Console.Clear();
+                }
+                else
+                {
+                    runProgram = false;
+                }          
+            }            
         }
     }
 }
